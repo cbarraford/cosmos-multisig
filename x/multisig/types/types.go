c918a0e104605417b227bdd17accddecb747b2c5
Chad Barraford <cbarraford@gmail.com>
2019-06-24 13:55:57 +0200

    generate wallet address

--- x/multisig/types/types.go
+++ x/multisig/types/types.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strings"
 
@@ -13,23 +14,39 @@ type MultiSigWallet struct {
 	Name     string         `json:"name"`
 	MinSigTx int            `json:"min_sig_tx"`
 	Address  sdk.AccAddress `json:"address"`
-	Keys     []string       `json:"pub_keys"`
+	PubKeys  []string       `json:"pub_keys"`
 }
 
 // Returns a new MultiSigWallet with the minprice as the price
-func NewMultiSigWallet(name string, keys []string, min int) MultiSigWallet {
+func NewMultiSigWallet(name string, pubKeys []string, min int) (MultiSigWallet, error) {
+	// encode name into a hex []byte
+	src := []byte(name)
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dst, src)
+
+	addr, err := sdk.AccAddressFromHex(string(dst))
+	if err != nil {
+		return MultiSigWallet{}, err
+	}
+
+	// check if we need to set a default min keys
+	if min < 2 {
+		min = len(pubKeys) - 1
+	}
+
 	return MultiSigWallet{
 		Name:     name,
 		MinSigTx: min,
-		Keys:     keys,
-	}
+		PubKeys:  pubKeys,
+		Address:  addr,
+	}, nil
 }
 
 // implement fmt.Stringer
 func (w MultiSigWallet) String() string {
 	return strings.TrimSpace(
 		fmt.Sprintf(
-			`Wallet: %s (%d of %d): %s`, w.Name, w.MinSigTx, len(w.Keys), w.Address,
+			`Wallet: %s (%d of %d): %s`, w.Name, w.MinSigTx, len(w.PubKeys), w.Address,
 		),
 	)
 }
